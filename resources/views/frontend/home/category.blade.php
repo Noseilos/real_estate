7798daf56aada42b78401511ab35f164f3e51d4b
Noseilos <jonaseguiron@gmail.com>
2023-06-24 12:44:35 +0800

    frontend: property type shown as property categories

--- resources/views/frontend/home/category.blade.php
+++ resources/views/frontend/home/category.blade.php
@@ -1,52 +1,30 @@
+@php
+    $property_type = App\Models\PropertyType::latest()->limit(5)->get();
+@endphp
+
+
 <section class="category-section centred">
     <div class="auto-container">
         <div class="inner-container wow slideInLeft animated" data-wow-delay="00ms" data-wow-duration="1500ms">
             <ul class="category-list clearfix">
+
+                @foreach ($property_type as $category)
+
+                @php
+                    $property = App\Models\Property::where('ptype_id', $category->id)->get();
+                @endphp
+                    
                 <li>
                     <div class="category-block-one">
                         <div class="inner-box">
-                            <div class="icon-box"><i class="icon-1"></i></div>
-                            <h5><a href="property-details.html">Residential</a></h5>
-                            <span>52</span>
+                            <div class="icon-box"><i class="{{ $category->type_icon }}"></i></div>
+                            <h5><a href="property-details.html">{{ $category->type_name }}</a></h5>
+                            <span>{{ count($property) }}</span>
                         </div>
                     </div>
                 </li>
-                <li>
-                    <div class="category-block-one">
-                        <div class="inner-box">
-                            <div class="icon-box"><i class="icon-2"></i></div>
-                            <h5><a href="property-details.html">Commercial</a></h5>
-                            <span>20</span>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="category-block-one">
-                        <div class="inner-box">
-                            <div class="icon-box"><i class="icon-3"></i></div>
-                            <h5><a href="property-details.html">Appertment</a></h5>
-                            <span>35</span>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="category-block-one">
-                        <div class="inner-box">
-                            <div class="icon-box"><i class="icon-4"></i></div>
-                            <h5><a href="property-details.html">Industrial</a></h5>
-                            <span>10</span>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="category-block-one">
-                        <div class="inner-box">
-                            <div class="icon-box"><i class="icon-5"></i></div>
-                            <h5><a href="property-details.html">Building Code</a></h5>
-                            <span>27</span>
-                        </div>
-                    </div>
-                </li>
+
+                @endforeach
             </ul>
             <div class="more-btn"><a href="categories.html" class="theme-btn btn-one">All Categories</a></div>
         </div>
