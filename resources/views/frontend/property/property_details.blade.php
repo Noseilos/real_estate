cb26f72dd42c0151f471da031736dfb1dbedad34
Noseilos <jonaseguiron@gmail.com>
2023-06-25 17:50:19 +0800

    frontend property details: dynamic property agent name, address, phone

--- resources/views/frontend/property/property_details.blade.php
+++ resources/views/frontend/property/property_details.blade.php
@@ -241,16 +241,34 @@
                         <div class="property-sidebar default-sidebar">
                             <div class="author-widget sidebar-widget">
                                 <div class="author-box">
-                                    <figure class="author-thumb"><img src="{{ asset('frontend/assets/images/resource/author-1.jpg')}}" alt=""></figure>
-                                    <div class="inner">
-                                        <h4>Michael Bean</h4>
-                                        <ul class="info clearfix">
-                                            <li><i class="fas fa-map-marker-alt"></i>84 St. John Wood High Street, 
-                                            St Johns Wood</li>
-                                            <li><i class="fas fa-phone"></i><a href="tel:03030571965">030 3057 1965</a></li>
-                                        </ul>
-                                        <div class="btn-box"><a href="agents-details.html">View Listing</a></div>
-                                    </div>
+
+                                    @if ($property->agent_id == NULL)
+
+                                        <figure class="author-thumb"><img src="{{ url('upload/admin.jpg') }}" alt=""></figure>
+                                        <div class="inner">
+                                            <h4>Admin</h4>
+                                            <ul class="info clearfix">
+                                                <li><i class="fas fa-map-marker-alt"></i>84 St. John Wood High Street, 
+                                                St Johns Wood</li>
+                                                <li><i class="fas fa-phone"></i><a href="tel:03030571965">030 3057 1965</a></li>
+                                            </ul>
+                                            <div class="btn-box"><a href="agents-details.html">View Listing</a></div>
+                                        </div>
+
+                                    @else
+
+                                        <figure class="author-thumb"><img src="{{ (!empty($property->user->photo)) ? url('upload/agent_images/'.$property->user->photo) : url('upload/no_image.jpg') }}" alt=""></figure>
+                                        <div class="inner">
+                                            <h4>{{ $property->user->name }}</h4>
+                                            <ul class="info clearfix">
+                                                <li><i class="fas fa-map-marker-alt"></i>{{ $property->user->address }}</li>
+                                                <li><i class="fas fa-phone"></i><a href="tel:03030571965">{{ $property->user->phone }}</a></li>
+                                            </ul>
+                                            <div class="btn-box"><a href="agents-details.html">View Listing</a></div>
+                                        </div>
+
+                                    @endif
+
                                 </div>
                                 <div class="form-inner">
                                     <form action="property-details.html" method="post" class="default-form">
