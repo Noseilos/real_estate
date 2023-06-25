b4f8f63d97a065cc65bd452362a84e5eba668afe
Noseilos <jonaseguiron@gmail.com>
2023-06-25 16:50:21 +0800

    frontend property details: dynamic amenities, details, location

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -17,8 +17,13 @@ class IndexController extends Controller
     public function PropertyDetails($id, $slug){
 
         $property = Property::findOrFail($id);
+
+        $amenities = Amenities::latest()->get();
+        $amenity_ids = explode(',', $property->amenities_id);
+        $property_amenities = $amenities->whereIn('id', $amenity_ids);
+
         $multiImage = MultiImage::where('property_id', $id)->get();
-        return view('frontend.property.property_details', compact('property', 'multiImage'));
+        return view('frontend.property.property_details', compact('property', 'multiImage', 'property_amenities'));
 
     }// END PropertyDetails
 
