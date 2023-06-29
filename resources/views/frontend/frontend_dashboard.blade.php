264d27075f3f7303439ed06ba17dd65a79e9679b
Noseilos <jonaseguiron@gmail.com>
2023-06-29 10:54:38 +0800

    Compare: compare table added

--- resources/views/frontend/frontend_dashboard.blade.php
+++ resources/views/frontend/frontend_dashboard.blade.php
@@ -297,5 +297,55 @@
     </script>
     {{-- End Load wishlist data --}}
 
+    {{-- Add to Compare --}}
+    <script>
+
+        function addToCompare(property_id){
+
+                $.ajax({
+                type: "POST",
+                dataType: 'json',
+                url: "/add-to-compare/"+property_id,
+
+                success:function(data){
+                    
+                    wishlist();
+
+                    // Start Message 
+
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        
+                        showConfirmButton: false,
+                        timer: 3000 
+                    })
+
+                    if ($.isEmptyObject(data.error)) {
+                            
+                            Toast.fire({
+                                type: 'success',
+                                icon: 'success', 
+                                title: data.success, 
+                            })
+
+                    }else{
+                    
+                            Toast.fire({
+                                type: 'error',
+                                icon: 'error', 
+                                title: data.error, 
+                            })
+                    }
+
+                    // End Message  
+
+                }
+            })
+        }
+
+    </script>
+    {{-- End Add to Compare --}}
+
 </body><!-- End of .page_wrapper -->
 </html>
--- resources/views/frontend/home/featured.blade.php
+++ resources/views/frontend/home/featured.blade.php
@@ -47,7 +47,7 @@
                                     <h4>${{ $item->lowest_price }}</h4>
                                 </div>
                                 <ul class="other-option pull-right clearfix">
-                                    <li><a href="property-details.html"><i class="icon-12"></i></a></li>
+                                    <li><a aria-label="Compare" class="action-btn" id="{{ $item->id }}" onclick="addToCompare(this.id)"><i class="icon-12"></i></a></li>
                                     <li><a aria-label="Add To Wishlist" class="action-btn" id="{{ $item->id }}" onclick="addToWishlist(this.id)"><i class="icon-13"></i></a></li>
                                 </ul>
                             </div>
