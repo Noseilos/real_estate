bdd433130a36a279ac1981802984759e49c459dc
Noseilos <jonaseguiron@gmail.com>
2023-07-15 18:56:09 +0800

    backend: property message layout updated

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="Responsive HTML Admin Dashboard Template based on Bootstrap 5">
-	<meta name="author" content="NobleUI">
-	<meta name="keywords" content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
 
-	<title>Real Estate</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Responsive HTML Admin Dashboard Template based on Bootstrap 5">
+    <meta name="author" content="NobleUI">
+    <meta name="keywords"
+        content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+
+    <title>Real Estate</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -17,142 +19,144 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
     <!-- End fonts -->
 
-	{{-- Select 2 --}}
-	<link rel="stylesheet" href="{{ asset('backend/assets/vendors/select2/select2.min.css') }}">
-  	<link rel="stylesheet" href="{{ asset('backend/assets/vendors/jquery-tags-input/jquery.tagsinput.min.css') }}">
+    {{-- Select 2 --}}
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/select2/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/jquery-tags-input/jquery.tagsinput.min.css') }}">
 
 
-	<!-- core:css -->
-	<link rel="stylesheet" href="{{ asset('backend/assets/vendors/core/core.css')}}">
-	<!-- endinject -->
+    <!-- core:css -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/core/core.css') }}">
+    <!-- endinject -->
 
-	<!-- Plugin css for this page -->
-	<link rel="stylesheet" href="{{ asset('backend/assets/vendors/flatpickr/flatpickr.min.css')}}">
-	<!-- End plugin css for this page -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/flatpickr/flatpickr.min.css') }}">
+    <!-- End plugin css for this page -->
 
-	<!-- inject:css -->
-	<link rel="stylesheet" href="{{ asset('backend/assets/fonts/feather-font/css/iconfont.css')}}">
-	<link rel="stylesheet" href="{{ asset('backend/assets/vendors/flag-icon-css/css/flag-icon.min.css')}}">
-	<!-- endinject -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/fonts/feather-font/css/iconfont.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+    <!-- endinject -->
 
-    <!-- Layout styles -->  
-        <link rel="stylesheet" href="{{ asset('backend/assets/css/demo2/style.css')}}">
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/demo2/style.css') }}">
     <!-- End layout styles -->
 
-    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.png')}}" />
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.png') }}" />
 
-	{{-- Toaster css--}}
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+    {{-- Toaster css --}}
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
 
-	{{-- Custom DataTables --}}
-	<link rel="stylesheet" href="{{ asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css')}}">
+    {{-- Custom DataTables --}}
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css') }}">
 </head>
+
 <body>
-	<div class="main-wrapper">
+    <div class="main-wrapper">
 
-		<!-- partial:partials/_sidebar.html -->
-		@include('admin.body.sidebar')
-		<!-- partial -->
-	
-		<div class="page-wrapper">
-					
-			<!-- partial:partials/_navbar.html -->
-			@include('admin.body.header')
-			<!-- partial -->
+        <!-- partial:partials/_sidebar.html -->
+        @include('admin.body.sidebar')
+        <!-- partial -->
 
-			@yield('admin')
+        <div class="page-wrapper">
 
-			<!-- partial:partials/_footer.html -->
-			@include('admin.body.footer')
-			<!-- partial -->
-		
-		</div>
-	</div>
+            <!-- partial:partials/_navbar.html -->
+            @include('admin.body.header')
+            <!-- partial -->
 
-	<!-- core:js -->
-	<script src="{{ asset('backend/assets/vendors/core/core.js')}}"></script>
-	<!-- endinject -->
+            @yield('admin')
 
-	<!-- Plugin js for this page -->
-  <script src="{{ asset('backend/assets/vendors/flatpickr/flatpickr.min.js')}}"></script>
-  <script src="{{ asset('backend/assets/vendors/apexcharts/apexcharts.min.js')}}"></script>
-	<!-- End plugin js for this page -->
+            <!-- partial:partials/_footer.html -->
+            @include('admin.body.footer')
+            <!-- partial -->
 
-	<!-- inject:js -->
-	<script src="{{ asset('backend/assets/vendors/feather-icons/feather.min.js')}}"></script>
-	<script src="{{ asset('backend/assets/js/template.js')}}"></script>
-	<!-- endinject -->
+        </div>
+    </div>
 
-	<!-- Custom js for this page -->
-  		<script src="{{ asset('backend/assets/js/dashboard-dark.js')}}"></script>
+    <!-- core:js -->
+    <script src="{{ asset('backend/assets/vendors/core/core.js') }}"></script>
+    <!-- endinject -->
 
-		{{-- Toaster --}}
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <!-- Plugin js for this page -->
+    <script src="{{ asset('backend/assets/vendors/flatpickr/flatpickr.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendors/apexcharts/apexcharts.min.js') }}"></script>
+    <!-- End plugin js for this page -->
 
-		<script>
-			@if(Session::has('message'))
-			var type = "{{ Session::get('alert-type','info') }}"
-			switch(type){
-			case 'info':
-			toastr.info(" {{ Session::get('message') }} ");
-			break;
-		
-			case 'success':
-			toastr.success(" {{ Session::get('message') }} ");
-			break;
-		
-			case 'warning':
-			toastr.warning(" {{ Session::get('message') }} ");
-			break;
-		
-			case 'error':
-			toastr.error(" {{ Session::get('message') }} ");
-			break; 
-			}
-			@endif 
-		</script>
-		{{-- End Toaster --}}
+    <!-- inject:js -->
+    <script src="{{ asset('backend/assets/vendors/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/template.js') }}"></script>
+    <!-- endinject -->
 
-		{{-- DataTables --}}
-			<script src="{{ asset('backend/assets/vendors/datatables.net/jquery.dataTables.js')}}"></script>
-			<script src="{{ asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js')}}"></script>
-			<script src="{{ asset('backend/assets/js/data-table.js')}}"></script>
-		{{-- End DataTables --}}
+    <!-- Custom js for this page -->
+    <script src="{{ asset('backend/assets/js/dashboard-dark.js') }}"></script>
 
-		{{-- Sweetalerts  --}}
-		<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-		<script src="{{ asset('backend/assets/js/code/code.js')}}"></script>
- 		<script src="{{ asset('backend/assets/js/code.js') }}"></script>
-		{{-- End Sweetalerts  --}}
+    {{-- Toaster --}}
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
-		{{-- Select 2 JS --}}
-		<script src="{{ asset('backend/assets/vendors/inputmask/jquery.inputmask.min.js') }}"></script>
-		<script src="{{ asset('backend/assets/vendors/select2/select2.min.js') }}"></script>
-		<script src="{{ asset('backend/assets/vendors/typeahead.js/typeahead.bundle.min.js') }}"></script>
+    <script>
+        @if (Session::has('message'))
+            var type = "{{ Session::get('alert-type', 'info') }}"
+            switch (type) {
+                case 'info':
+                    toastr.info(" {{ Session::get('message') }} ");
+                    break;
 
-		<script src="{{ asset('backend/assets/vendors/jquery-tags-input/jquery.tagsinput.min.js') }}"></script>
+                case 'success':
+                    toastr.success(" {{ Session::get('message') }} ");
+                    break;
 
-		<script src="{{ asset('backend/assets/js/inputmask.js') }}"></script>
-		<script src="{{ asset('backend/assets/js/select2.js') }}"></script>
-		<script src="{{ asset('backend/assets/js/typeahead.js') }}"></script>
-		<script src="{{ asset('backend/assets/js/tags-input.js') }}"></script>
-		{{-- End Select 2 --}}
+                case 'warning':
+                    toastr.warning(" {{ Session::get('message') }} ");
+                    break;
 
-		{{-- Tinymice --}}
-		<script src="{{ asset('backend/assets/vendors/tinymce/tinymce.min.js') }}"></script>
-     	<script src="{{ asset('backend/assets/js/tinymce.js') }}"></script>
-		{{-- End Tinymice --}}
+                case 'error':
+                    toastr.error(" {{ Session::get('message') }} ");
+                    break;
+            }
+        @endif
+    </script>
+    {{-- End Toaster --}}
 
-		{{-- Custom Validation  --}}
-		<script src="{{ asset('backend/assets/js/code/validate.min.js')}}"></script>
-		{{-- End Custom Validation  --}}
+    {{-- DataTables --}}
+    <script src="{{ asset('backend/assets/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/data-table.js') }}"></script>
+    {{-- End DataTables --}}
 
-		{{-- Start Toggle --}}
-		<script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
-		{{-- End Toggle --}}
+    {{-- Sweetalerts  --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="{{ asset('backend/assets/js/code/code.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/code.js') }}"></script>
+    {{-- End Sweetalerts  --}}
 
-	<!-- End custom js for this page -->
-	
+    {{-- Select 2 JS --}}
+    <script src="{{ asset('backend/assets/vendors/inputmask/jquery.inputmask.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendors/select2/select2.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendors/typeahead.js/typeahead.bundle.min.js') }}"></script>
+
+    <script src="{{ asset('backend/assets/vendors/jquery-tags-input/jquery.tagsinput.min.js') }}"></script>
+
+    <script src="{{ asset('backend/assets/js/inputmask.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/select2.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/typeahead.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/tags-input.js') }}"></script>
+    {{-- End Select 2 --}}
+
+    {{-- Tinymice --}}
+    <script src="{{ asset('backend/assets/vendors/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/tinymce.js') }}"></script>
+    {{-- End Tinymice --}}
+
+    {{-- Custom Validation  --}}
+    <script src="{{ asset('backend/assets/js/code/validate.min.js') }}"></script>
+    {{-- End Custom Validation  --}}
+
+    {{-- Start Toggle --}}
+    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+    {{-- End Toggle --}}
+
+    <!-- End custom js for this page -->
+
 
 </body>
-</html>    
+
+</html>
--- resources/views/agent/message/message_details.blade.php
+++ resources/views/agent/message/message_details.blade.php
@@ -3,59 +3,11 @@
 
     <div class="page-content">
         <div class="row inbox-wrapper">
-            <div class="col-lg-12">
+            <div class="col-lg-9">
                 <div class="card">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-lg-3 border-end-lg">
-                                <div class="d-flex align-items-center justify-content-between">
-                                    <button class="navbar-toggle btn btn-icon border d-block d-lg-none"
-                                        data-bs-target=".email-aside-nav" data-bs-toggle="collapse" type="button">
-                                        <span class="icon"><i data-feather="chevron-down"></i></span>
-                                    </button>
-                                    <div class="order-first">
-                                        <h4>Mail Service</h4>
-                                        <p class="text-muted">support@nosearealestate.com</p>
-                                    </div>
-                                </div>
-                                <div class="d-grid my-3">
-                                    <a class="btn btn-primary" href="./compose.html">Compose Email</a>
-                                </div>
-                                <div class="email-aside-nav collapse">
-                                    <ul class="nav flex-column">
-                                        <li class="nav-item active">
-                                            <a class="nav-link d-flex align-items-center"
-                                                href="{{ route('agent.property.message') }}">
-                                                <i data-feather="inbox" class="icon-lg me-2"></i>
-                                                Inbox
-                                                <span class="badge bg-danger fw-bolder ms-auto">{{ count($user_message) }}
-                                            </a>
-                                        </li>
-                                    </ul>
-                                    <p class="text-muted tx-12 fw-bolder text-uppercase mb-2 mt-4">Labels</p>
-                                    <ul class="nav flex-column">
-                                        <li class="nav-item">
-                                            <a class="nav-link d-flex align-items-center" href="#">
-                                                <i data-feather="tag" class="text-warning icon-lg me-2"></i>
-                                                Important
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link d-flex align-items-center" href="#">
-                                                <i data-feather="tag" class="text-primary icon-lg me-2"></i>
-                                                Business
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link d-flex align-items-center" href="#">
-                                                <i data-feather="tag" class="text-info icon-lg me-2"></i>
-                                                Inspiration
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-lg-9">
+                            <div class="col-lg-12">
                                 <div class="p-3 border-bottom">
                                     <div class="row align-items-center">
                                         <div class="col-lg-6">
