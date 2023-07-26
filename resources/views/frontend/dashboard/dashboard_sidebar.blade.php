678061b7bcbf4be9b92102e5593486356555ec3f
anwisu <cabauatan.jianellaguia@gmail.com>
2023-07-26 19:25:35 +0800

    user dashboard: sidebar icons update

--- resources/views/frontend/dashboard/dashboard_sidebar.blade.php
+++ resources/views/frontend/dashboard/dashboard_sidebar.blade.php
@@ -2,10 +2,10 @@
     <ul class="category-list ">
         
     <li><a href="{{ route('user.profile') }}"><i class="fa fa-cog" aria-hidden="true"></i> Settings</a></li>
-    <li><a href="{{ route('user.schedule.request') }}"><i class="fa fa-credit-card" aria-hidden="true"></i> Schedule Request <span class="badge badge-info">(  )</span></a></li>
+    <li><a href="{{ route('user.schedule.request') }}"><i class="fa fa-calendar" aria-hidden="true"></i> Schedule Request <span class="badge badge-info">(  )</span></a></li>
     <li><a href="{{ route('user.compare') }}"><i class="fa fa-list-alt" aria-hidden="true"></i></i> Compare </a></li>
-    <li><a href="{{ route('user.wishlist') }}"><i class="fa fa-indent" aria-hidden="true"></i> Wishlist  </a></li>
-    <li><a href="{{ route('live.chat') }}"><i class="fa fa-indent" aria-hidden="true"></i> Live Chat  </a></li>
+    <li><a href="{{ route('user.wishlist') }}"><i class="fa fa-heart" aria-hidden="true"></i> Wishlist  </a></li>
+    <li><a href="{{ route('live.chat') }}"><i class="fa fa-comment-dots" aria-hidden="true"></i> Live Chat  </a></li>
     <li><a href="{{ route('user.change.password') }}"><i class="fa fa-key" aria-hidden="true"></i> Change Password </a></li>
     <li><a href="{{ route('user.logout') }}"><i class="fa fa-chevron-circle-up" aria-hidden="true"></i> Logout </a></li>
     </ul>
